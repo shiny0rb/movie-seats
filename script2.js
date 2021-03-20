e87bb41162c40af13a11cfc2feeb98797645105e
shiny0rb <teacherrossvn@gmail.com>
2021-03-20 21:42:00 +0700

    adding the files from the directory

---
+++ script2.js
@@ -0,0 +1,25 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+const ticketPrice = +movieSelect.value;
+
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row.seat.selected');
+  
+  console.log(selectedSeats);
+}
+
+container.addEventListener('click', e => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
+  }
+});
